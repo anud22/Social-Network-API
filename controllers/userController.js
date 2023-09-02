f82eb413b8b4a6af255526e818914cc05d79c041
anuD <adilbagi22@gmail.com>
2023-09-02 11:53:54 -0700

    Added users PUT route coder

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,4 +48,24 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  
+  //Update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
 };
